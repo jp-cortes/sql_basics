3d81943b2a79d26adbeb52c5910c139d056d1a54
JP <alias99002@outlook.com>
2023-05-05 09:05:43 -0500

    WHERE  IS NULL, IS NOT NULL

--- Where.sql
+++ Where.sql
@@ -2,31 +2,46 @@
 
 SELECT	*
 FROM		posts
-WHERE	id	< 50;
+WHERE	id	< 50; -- id lower than 50
+
+SELECT *
+FROM posts
+WHERE id!=50;-- !== is some cases is not working so is possible use "<>" means  Different to ...
 
 SELECT	*
 FROM		posts
-WHERE	status = 'Inactivo';
+WHERE	status = 'Inactivo';-- status equals to 
 
 SELECT	*
 FROM		posts
-WHERE	title LIKE '%scandal%';
+WHERE	title LIKE '%escandalo%';-- post that in  the title  contains the word "escandalo"
+-- example 2
+WHERE	title LIKE 'escandalo%';-- post which title  begins with the word "escandalo"
+-- example 3
+WHERE	title LIKE '%escandalo';-- post which title  ends with the word "escandalo"
+--NOT LIKE is also possible to use
 
 SELECT	*
 FROM		posts
-WHERE	publication_date > '2025-01-01';
+WHERE	publication_date > '2025-01-01';--date lower than
 
 SELECT	*
 FROM		posts
-WHERE	publication_date BETWEEN '2023-01-01' AND '2025-12-31';
+WHERE	publication_date BETWEEN '2023-01-01' AND '2025-12-31';--between
 
 SELECT	*
 FROM		posts
-WHERE	YEAR(publication_date) BETWEEN '2023' AND '2024';
+WHERE	YEAR(publication_date) BETWEEN '2023' AND '2024';--year
+--NOT BETWEEN "" AND"" is also possible to use
 
 SELECT	*
 FROM		posts
-WHERE	MONTH(publication_date) = '04';
+WHERE	MONTH(publication_date) = '04';--month
+
+
+SELECT	*
+FROM		posts
+WHERE	user_id IS NULL;
 
 SELECT	*
 FROM		posts
@@ -34,4 +49,18 @@ WHERE	user_id IS NOT NULL;
 
 SELECT	*
 FROM		posts
-WHERE	user_id IS NULL;
+WHERE	user_id IS NOT NULL
+AND estatus = 'activo'
+AND id < 50
+AND categoria_id=2
+AND YEAR(fecha_publicacion)=2025
+;
+
+REGEXP_LIKE -- for REGEX
+
+SELECT *
+FROM        post
+WHERE   REGEXP_LIKE (users.id ,'[0-5]')--REGEX
+;
+
+
